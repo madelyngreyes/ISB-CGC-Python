4150420d6bc19b89211cb27f021853e6a43eabdb
pihltd <todd.pihl@gmail.com>
2017-03-03 21:31:51 +0000

    further work

--- gene2BamSlice.py
+++ gene2BamSlice.py
@@ -9,22 +9,41 @@ import pprint
 import requests
 import json
 from google.cloud import bigquery
+import httplib2
+import datetime
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client import tools
+from oauth2client.file import Storage
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+DEFAULT_STORAGE_FILE = os.path.join(os.path.expanduser("~"), '.isb_credentials')
+DEFAULT_PROJECT = 'cgc-05-0016'
+
+def runSyncQuery(query, parameters, project):
+	bq = bigquery.Client(project=project)
+	if parameters is not None:
+		query_results = bq.run_sync_query(query, query_parameters = [parameters])
+	else:
+		query_results = bq.run_sync_query(query)
+	query_results.use_legacy_sql = False
+	query_results.run()
+	return query_results
+
+def getSampleList(study):
+	query = "SELECT SampleBarcode FROM `isb-cgc.tcga_cohorts.@study` LIMIT 10"
+	query_parameters = (bigquery.ScalarQueryParameter('study', 'STRING', study))
+	results = runSyncQuery(query, query_parameters)
 
 def getGenomicLocation(gene,verbose):
 	#Useful links for queries:
 	#https://cloud.google.com/bigquery/docs/reference/standard-sql/migrating-from-legacy-sql#escaping_reserved_keywords_and_invalid_identifiers
 	#https://media.readthedocs.org/pdf/google-cloud-python/latest/google-cloud-python.pdf   Search on ScalarQueryParameter to get syntax
-	bq = bigquery.Client()
 	query = "SELECT seq_name, start, `end` FROM `isb-cgc.genome_reference.GENCODE_v24` WHERE feature = 'gene' AND gene_Status = 'KNOWN' AND gene_name = @gene;"
-
 	query_parameters = (
 		bigquery.ScalarQueryParameter('gene', 'STRING', gene)
 	)
-
-	query_results = bq.run_sync_query(query,query_parameters = [query_parameters])
-
-	query_results.use_legacy_sql = False
-	query_results.run()
+	query_results = runSyncQuery(query, query_parameters, DEFAULT_PROJECT)
 
 	page_token = None
 	while True:
@@ -38,26 +57,59 @@ def getGeneList(table, genecount, verbose):
 	#This may be a bit stupid, but this routine will return gene names from any table with the column "gene_name"
 	query = "SELECT gene_name FROM @table"
 	query_parameters = (bigquery.ScalarQueryParameter('table', 'STRING', table))
+	vPrint(verbose, ("Table name is %s" % table))
+	vPrint(verbose, ("Limit is %s" % str(genecount)))
 
 	if genecount is not None:
 		#query = "SELECT gene_name FROM @table LIMIT @genecount"
-		query = "SELECT gene_name FROM `cgc-05-0016.other.tdp_hg38_genex_cnv` LIMIT 10"
-		query_parameters = (
-			bigquery.ScalarQueryParameter('table', 'STRING', table),
-			bigquery.ScalarQueryParameter('genecount', 'INTEGER', genecount)
-		)
-	bq = bigquery.Client()
-	#query_results = bq.run_sync_query(query,query_parameters = [query_parameters])
-	query_results = bq.run_sync_query(query)
-	query_results.use_legacy_sql = False
-	query_results.run()
-
+		query = "SELECT gene_name FROM @table LIMIT 10"
+		query_parameters = (bigquery.ScalarQueryParameter('table', 'STRING', table))
+		#query = "SELECT gene_name FROM `isb-cgc.other.tdp_hg38_genex_cnv` LIMIT @genecount"
+		#query = "SELECT gene_name FROM other.tdp_hg38_genex_cnv LIMIT @genecount"
+		#query_parameters = (bigquery.ScalarQueryParameter('genecount', 'INTEGER', genecount))
+		#query_parameters = (
+		#	bigquery.ScalarQueryParameter('table', 'STRING', table),
+		#	bigquery.ScalarQueryParameter('genecount', 'INTEGER', genecount)
+		#)
+	query_results = runSyncQuery(query, query_parameters, DEFAULT_PROJECT)
 	page_token = None
 	while True:
 		rows, total_rows, page_token = query_results.fetch_data(page_token=page_token)
 		return rows
 		if not page_token:
 			break
+
+def getFileLocations(barcode):
+	api = 'isb_cgc_api'
+	version = 'v2'
+	site = "https://api-dot-isb-cgc.appspot.com"
+	credentials = get_credentials(None)
+	service = get_authorized_service(api,version,site,credentials,args.verbose)
+	data = service.samples().cloud_storage_file_paths(sample_barcode=barcode).execute()
+	pprint.pprint(data)
+
+def get_authorized_service(api, version, site, credentials, verbose):
+	discovery_url = '%s/_ah/api/discovery/v1/apis/%s/%s/rest' % (site, api, version)
+	vprint(verbose, discovery_url)
+	http = credentials.authorize(httplib2.Http())
+	if credentials.access_token_expired or credentials.invalid:
+		credentials.refresh(http)
+	authorized_service = build(api, version, discoveryServiceUrl=discovery_url, http=http)
+	return authorized_service
+
+def get_credentials(credFile):
+	oauth_flow_args = ['--noauth_local_webserver']
+	if credFile is None:
+		storage = Storage(DEFAULT_STORAGE_FILE)
+	else:
+		storage = Storage(credFile)
+
+	credentials = storage.get()
+	if not credentials or credentials.invalid:
+		flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, EMAIL_SCOPE)
+		flow.auth_uri = flow.auth_uri.rstrip('/') + '?approval_prompt=force'
+		credentials = tools.run_flow(flow, storage, tools.argparser.parse_args(oauth_flow_args))
+	return credentials
 
 def vPrint(doit, message):
 	if doit:
@@ -80,6 +132,9 @@ def main(args):
 	else:
 		print "No gene file or table provided, exiting"
 		sys.exit()
+	#Get the patient list
+	if args.studyname is not None:
+		sampleList = getSamples(args.studyname)
 
 	for gene in genelist:
 		if args.genefile is not None:
