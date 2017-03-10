554900fd8f35d07b0ea48d61c14c2210a86ffd10
pihltd <todd.pihl@gmail.com>
2017-03-10 10:19:06 -0500

    Refined jq parsing

--- ssh_mysql_qa.sh
+++ ssh_mysql_qa.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+#####################################################
+#                                                   #
+# Requires Google Cloud SDK and jq to be installed  #
+# jq: https://stedolan.github.io/jq/                #
+# SDK: https://cloud.google.com/sdk/downloads       #
+#                                                   #
+#####################################################
 
-usage() { echo "Usage: $0 [-v <VM name>] [-u <username>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-v <VM name>] [-u <username>] [-p <port number> Default is 3306] [-n No tunnel]" 1>&2; exit 1; }
 
-VMNAME="<DEFAULT VM NAME HERE"
+VMNAME="DEFAULT VM NAME HERE"
 USER="DEFAULT USER NAME HERE"
+PORT=3306
+TUNNEL="True"
 
-while getopts "v:u:" args; do
+while getopts "v:u:p:n" args; do
 	case "${args}" in
 		v)
 			VMNAME=${OPTARG}
@@ -13,21 +22,31 @@ while getopts "v:u:" args; do
 		u)
 			USER=${OPTARG}
 			;;
+		p)
+			PORT=${OPTARG}
+			;;
+		n)
+			TUNNEL="False"
+			;;
 		*)
 			usage
 			;;
 	esac
 done
-	  
-COUNT=0
-INSTANCES=$(gcloud compute instances list --format=json | jq '.[] | {name: .name, ip: .networkInterfaces[].accessConfigs[].natIP'})
+
+INSTANCES=$(gcloud compute instances list --format=json | jq '.[] | .name,.networkInterfaces[].accessConfigs[].natIP')
 IFS='"' read -ra ARRAY <<< ${INSTANCES}
-for i in "${ARRAY[@]}"; do
-	if [[ "${i}" == "${VMNAME}" ]]; then
-		((COUNT+=4))
-		IP="${ARRAY[$COUNT]}"
+
+for ((i=0; i<${#ARRAY[*]};i++)); do
+	if [[ "${ARRAY[i]}" == "${VMNAME}" ]]; then
+		IP="${ARRAY[$((i+2))]}"
 	fi
-	((COUNT+=1))
 done
 
-ssh ${USER}@${IP} -L 3306:localhost:3306
+exit
+
+if [[ "${TUNNEL}" -eq "True" ]] ; then
+	ssh ${USER}@${IP} -L ${PORT}:localhost:${PORT}
+else
+	ssh ${USER}@${IP}
+fi
